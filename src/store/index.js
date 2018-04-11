bb2165963d0855e8fd77e4f30017da6c326cab9d
Samantha Lau <slau@jirnexu.com>
2018-04-11 18:18:04 +0800

    Setup store

---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const state = {
+  events: [],
+  test: ""
+};
+
+const mutations = {
+  addEvent(state, payload) {
+    state.test = "Aloha";
+
+    // let event = {
+    //   date: new Date(payload.eventDate),
+    //   title: payload.eventTitle,
+    //   details: payload.eventDetails
+    // };
+    console.log(payload);
+    console.log(state);
+    state.events.push(payload);
+  }
+};
+
+export default new Vuex.Store({
+  state,
+  mutations
+});
